d1029b888b9b3044eccdea042bdb54872e253e66
Your Name <you@example.com>
2024-12-20 17:18:35 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import MyComponent from './ProxzarLoader';
+
+function App() {
+  return (
+    <div className="App">
+    
+      <MyComponent/>
+    </div>
+  );
+}
+
+export default App;
